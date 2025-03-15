9131606f6d911dfedc8c496b513ca916aca6bc0f
M-Fathul <mfathulmubiin@gmail.com>
2025-03-15 10:18:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,6 @@ st.info('Hello world!')
 with st.expander('Data'):
   df = pd.read_csv('https://raw.githubusercontent.com/M-Fathul/startingML/refs/heads/master/calonpembelimobil.csv')
   df
-  st.write('**Class**')
+  print('**Class**')
+  x = df.drop(
 
