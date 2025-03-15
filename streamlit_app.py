00035730792eb67c870e20bbc4c5ef56a67338b6
M-Fathul <mfathulmubiin@gmail.com>
2025-03-15 13:17:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,8 @@ with st.sidebar:
   Status = st.selectbox('Status', (0, 1, 2, 3))
   Kelamin = st.selectbox('Status', (0, 1))
   Usia = st.number_input("Insert age", value=None, placeholder="Type a number...")
+  Memiliki_Mobil = st.number_input("car did you have", value=None, placeholder="Type a amount...")
+  Penghasilan = st.number_input("sallary", value=None, placeholder="Type a amount...")
+  data = {'Status': Status, 'Kelamin': Kelamin, 'Usia': Usia, 'Memiliki_Mobil': Memiliki_Mobil, 'Penghasilan': Penghasilan}
+  input_df = pd.DataFrame(data, index=[0])
+input_df
