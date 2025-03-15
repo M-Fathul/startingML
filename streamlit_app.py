e8fd561f508c9ea12e27a1cc65b77d6a178dbe94
M-Fathul <mfathulmubiin@gmail.com>
2025-03-15 10:37:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
-
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+import seaborn as sns
 st.title('🎈 App Name')
 
 st.info('Hello world!')
@@ -15,4 +20,13 @@ with st.expander('Data'):
   st.write('**Class**')
   y = df.Beli_Mobil
   y
+  st.bar_chart(df, x="Beli_Mobil", y="yield", color="site", stack=False)
+  kor = df[['Usia', 'Memiliki_Mobil', 'Penghasilan']]
+  korelasi = kor.corr()
+  sns.heatmap(korelasi, annot=True, cmap='coolwarm')
+  plt.title('Heatmap Korelasi')
+  plt.show()
+  sns.countplot(x=df['Beli_Mobil'])
+  plt.title('Distribusi Variabel Target')
+  plt.show()
 
