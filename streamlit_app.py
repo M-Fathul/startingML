be0f661c90b32e1edd0c149fea3b0c1db1c96ffa
M-Fathul <mfathulmubiin@gmail.com>
2025-03-15 10:09:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ import pandas as pd
 st.title('🎈 App Name')
 
 st.info('Hello world!')
+
+df = pd.read_csv('https://raw.githubusercontent.com/M-Fathul/startingML/refs/heads/master/calonpembelimobil.csv')
+df
