005d5201eb7d4025cba434c2160887ce27c3e39b
M-Fathul <mfathulmubiin@gmail.com>
2025-03-15 11:24:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,4 @@ with st.expander('Data'):
 with st.sidebar:
   Status = st.selectbox('Status', (0, 1, 2, 3))
   Kelamin = st.selectbox('Status', (0, 1))
-  Usia = st.slider("How old are you?", 0, 167, 0)
+  Usia = st.number_input("Insert age", value=None, placeholder="Type a number...")
